4f290aa33ce6d9c1ca5e46527855ca6219fa6cf2
Arshad-Mhr <125758052+Arshad-Mhr@users.noreply.github.com>
2023-03-01 18:53:30 +0500

    Add files via upload

---
+++ importing_entire_module.py
@@ -0,0 +1,2 @@
+# A module is a file ending in .py that contains the code you want to import into your Functions program.
+
---
+++ making_pizza.py
@@ -0,0 +1,10 @@
+# import pizaa as p
+from pizaa import*
+# from pizaa import make_pizza as mp
+
+
+make_pizza(16, 'mushroom', 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# Importing All Functions in a Module
+# You can tell Python to import every function in a module by using the asterisk (*) operator:
